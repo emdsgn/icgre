f064302a49e5fe3500cb905f417646200ac95e43
administrator@international-aset.com <ICNFAicepr1120>
2018-03-16 09:07:02 -0400

    Fix registration form link

--- registrationform.php
+++ registrationform.php
@@ -43,7 +43,7 @@ $message = 'Dear Colleague,
 
 Thank you for registering for ICGRE 2018. If you have requested any official letters, please allow up to 5 business days to receive your documents.
 
-If you are an author, please make sure to send us the final version of your paper and a signed copyright form via email to info@icgre.org. You can find the copyright form here: www.icgre.org/papers. Please note that failing to do so may result in an unsuccessful process of your registration.
+If you are an author, please make sure to send us the final version of your paper and a signed copyright form via email to info@icgre.org. You can find the copyright form here: https://icgre.org/papers. Please note that failing to do so may result in an unsuccessful process of your registration.
 
 To reserve a room with a DISCOUNTED price, please fill out the provided booking form here: https://icgre.org/accommodation/#Novotel
 
